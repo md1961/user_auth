605877ac8c461e7399890066a92f02c499d69222
Naoyuki Kumagai <kumagai@uranus>
2011-01-24 15:41:44 +0900

    Created lib/app/controllers/sessions_controller.rb
    .gitignore
    - Newly created.
    init.rb
    - Added "require 'user_auth'".
    lib/app/controllers/sessions_controller.rb
    - Newly created (moved from the Rails project above).
    lib/user_auth.rb
    - Added 'lib/app/controllers' to autoload paths.

---
+++ lib/app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+# モデル Session の処理を行うコントローラ
+class SessionsController < ApplicationController
+
+  # ログイン認証を行い、成功すれば Session にユーザIDを設定する。失敗した場合はログイン画面に戻る<br />
+  # <em>params[:name]</em> : ユーザ名<br />
+  # <em>params[:password]</em> : パスワード
+  def create
+    if user = User.authenticate(params[:name], params[:password])
+      session[:user_id] = user.id
+      redirect_to root_path
+    else
+      flash.now[:alert] = t("helpers.notice.session.invalid_login")
+      render :action => 'new'
+    end
+  end
+
+  # Session のデータをクリアし、ログイン画面に戻る
+  def destroy
+    reset_session
+    redirect_to root_path, :notice => t("helpers.notice.session.logged_out")
+  end
+end
