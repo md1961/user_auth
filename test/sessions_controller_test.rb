55f323b2e315ced1829929a70c9f3874a6a178f0
Naoyuki Kumagai <kumagai@uranus>
2011-01-25 11:55:52 +0900

    Added test_create_for_authenticated() to SessionsControllerTest
    test/sessions_controller_test.rb
    - Added test_create_for_authenticated(), replacing test_new().
    - Added class User definition to override User.authenticate().

--- test/sessions_controller_test.rb
+++ test/sessions_controller_test.rb
@@ -11,9 +11,39 @@ class SessionsControllerTest < ActionController::TestCase
     end
   end
 
-  def test_new
-    get :new
-    assert_response :success
+  USER_ID = 345
+
+  def test_create_for_authenticated
+    user_mock = Object.new
+    def user_mock.id
+      USER_ID
+    end
+
+    User.override_authenticate(user_mock)
+    get :create
+    User.restore_authenticate
+    assert_equal(USER_ID, session[:user_id], "session[:user_id]")
+    assert_redirected_to '/'
+  end
+end
+
+
+class User
+  def self.override_authenticate(result)
+    @@__kuma_result__ = result
+    User.instance_eval do
+      alias :authenticate_orig :authenticate
+      alias :authenticate      :authenticate_overriden
+    end
+  end
+  def self.restore_authenticate
+    User.instance_eval do
+      alias :authenticate :authenticate_orig
+    end
+  end
+
+  def self.authenticate_overriden(name, password)
+    return @@__kuma_result__
   end
 end
 
