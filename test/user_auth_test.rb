b5a391ccdad2818f9d6b369a049689a130c78cd1
Naoyuki Kumagai <kumagai@uranus>
2011-02-03 09:53:04 +0900

    Implemented UserAuthTest for ActiveSupport::Dependencies.autoload_paths
    version=0.0.1.4
    [plugins: 26 tests, 83 assertions, 0 failures, 0 errors, 0 skips in 0.4 seconds]
    test/user_auth_test.rb
    - Added test_autoload_paths().

--- test/user_auth_test.rb
+++ test/user_auth_test.rb
@@ -1,8 +1,14 @@
 require 'test_helper'
 
 class UserAuthTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+
+  def test_autoload_paths
+    autoload_paths = ActiveSupport::Dependencies.autoload_paths
+
+    DIRECTORIES_TO_LOAD_FROM.each do |dir|
+      dir_fullpath = File.expand_path("#{File.dirname(__FILE__)}/../lib/app/#{dir}")
+      assert(autoload_paths.include?(dir_fullpath), "'{dir_fullpath}' should be in autoload_paths")
+    end
   end
 end
+
