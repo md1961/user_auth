17797a1f91fb51ff376482a95d8fcb6386f89f7d
Naoyuki Kumagai <kumagai@uranus>
2011-02-07 17:15:03 +0900

    Added test_update_password_for_successful_update to 'users_controller_test.rb'
    version=0.0.2.5
    [plugins: 29 tests, 93 assertions, 0 failures, 0 errors, 0 skips in 0.4 seconds]
    test/users_controller_test.rb
    - Modified setup() to prepare root path.
    - Added test_update_password_for_successful_update().

--- test/users_controller_test.rb
+++ test/users_controller_test.rb
@@ -8,7 +8,7 @@ class TestTargetControllerTest < ActionController::TestCase
 
   def setup
     Rails.application.routes.draw do
-      root :to => "test_target#index"
+      root :to => 'test_target#change_password'
 
       match ':controller(/:action(/:id(.:format)))'
     end
@@ -22,27 +22,7 @@ class TestTargetControllerTest < ActionController::TestCase
   end
 
   def test_update_password_for_wrong_old_password
-    errors_mock = Object.new
-    def errors_mock.add(column_name, message)
-      @column_name = column_name
-      @message     = message
-    end
-    def errors_mock.column_name
-      @column_name
-    end
-    def errors_mock.message
-      @message
-    end
-
-    user_mock = User.new
-    user_mock.instance_variable_set(:@errors, errors_mock)
-    def user_mock.authenticated?(old_password)
-      false
-    end
-    def user_mock.errors
-      @errors
-    end
-
+    user_mock = make_user_mock_not_to_be_authenticated
     @controller.instance_variable_set(:@current_user, user_mock)
 
     get :update_password, :user => {}
@@ -54,5 +34,56 @@ class TestTargetControllerTest < ActionController::TestCase
     assert_equal(:old_password, current_user.errors.column_name  , "column_name of @current_user.erros")
     assert_equal(String       , current_user.errors.message.class, "Class of message of @current_user.erros")
   end
+
+    def make_user_mock_not_to_be_authenticated
+      errors_mock = Object.new
+      def errors_mock.add(column_name, message)
+        @column_name = column_name
+        @message     = message
+      end
+      def errors_mock.column_name
+        @column_name
+      end
+      def errors_mock.message
+        @message
+      end
+
+      user_mock = User.new
+      user_mock.instance_variable_set(:@errors, errors_mock)
+      def user_mock.authenticated?(old_password)
+        false
+      end
+      def user_mock.errors
+        @errors
+      end
+
+      return user_mock
+    end
+    private :make_user_mock_not_to_be_authenticated
+
+  def test_update_password_for_successful_update
+    user_mock = make_user_mock_to_be_updated_successfully
+    @controller.instance_variable_set(:@current_user, user_mock)
+
+    get :update_password, :user => {}
+    current_user = assigns(:current_user)
+
+    assert_redirected_to root_path
+    assert_equal(User  , current_user.class  , "Class of @current_user")
+    assert_equal(String, flash[:notice].class, "Class of flash[:notice]")
+  end
+
+    def make_user_mock_to_be_updated_successfully
+      user_mock = User.new
+      def user_mock.authenticated?(old_password)
+        true
+      end
+      def user_mock.update_attributes(params)
+        true
+      end
+
+      return user_mock
+    end
+    private :make_user_mock_to_be_updated_successfully
 end
 
