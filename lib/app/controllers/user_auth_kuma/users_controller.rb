afa840091c6f9339e6515fd7d4d99e08abd63d84
Naoyuki Kumagai <kumagai@uranus>
2011-02-07 13:39:49 +0900

    Created UserAuthKuma::UsersController which took over update_password(), etc.
    version=0.0.2.0
    [plugins: 26 tests, 83 assertions, 0 failures, 0 errors, 0 skips in 0.4 seconds]
    lib/app/controllers/user_auth_kuma/users_controller.rb
    - Newly created as UserAuthKuma::UsersController, taking over change_password()
      and update_password() from UsersController of Rails root.

---
+++ lib/app/controllers/user_auth_kuma/users_controller.rb
@@ -0,0 +1,27 @@
+module UserAuthKuma
+
+# モデル User の処理を行うコントローラ
+class UsersController < ApplicationController
+
+  # パスワード変更画面を表示する
+  def change_password
+    # just render
+  end
+
+  # パスワードを更新する<br />
+  # <em>params[:user][:old_password]</em> : ユーザ確認のための現在のパスワード<br />
+  # <em>params[:user]</em> : パスワード（２度入力）を保持する Hash
+  def update_password
+    if ! @current_user.authenticated?(params[:user][:old_password])
+      @current_user.errors.add(:old_password, t("helpers.alert.user.old_password_not_match"))
+      render :action => 'change_password'
+    elsif @current_user.update_attributes(params[:user])
+      redirect_to projects_path, :notice => t("helpers.notice.user.updated")
+    else
+      render :action => 'change_password'
+    end
+  end
+end
+
+end
+
