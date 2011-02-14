7beaa0915228da4a643e0636fb0c55c5b5cb7742
Naoyuki Kumagai <kumagai@uranus>
2011-02-14 15:17:24 +0900

    Extracted a helper from UsersHelperTest#test_attribute_value()
    version=0.0.7.4
    [plugins: 40 tests, 129 assertions, 0 failures, 0 errors, 0 skips in 0.7 seconds]
    test/users_helper_test.rb
    - Extracted make_user_mock_to_test_attribute_value() from
      test_attribute_value().

--- test/users_helper_test.rb
+++ test/users_helper_test.rb
@@ -4,16 +4,22 @@ class UsersHelperTest < ActionView::TestCase
   include UserAuthKuma::UsersHelper
 
   def test_attribute_value
-    user_mock = Object.new
-    value = true
-    user_mock.instance_variable_set(:@name, value)
-    def user_mock.attributes
-      return {'name' => @name}
-    end
+    user_mock = make_user_mock_to_test_attribute_value(:name, true)
 
     expected = UserAuthKuma::UsersHelper::YES_DISPLAY
     actual   = attribute_value(user_mock, :name)
     assert_equal(expected, actual, "for value of true")
   end
+
+    def make_user_mock_to_test_attribute_value(attr_name, attr_value)
+      user_mock = Object.new
+      user_mock.instance_variable_set(:@name , attr_name )
+      user_mock.instance_variable_set(:@value, attr_value)
+      def user_mock.attributes
+        {@name.to_s => @value}
+      end
+
+      return user_mock
+    end
 end
 
