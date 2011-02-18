e34a56d7a41c2ee5f36792014f64d80082852491
Naoyuki Kumagai <kumagai@uranus>
2011-02-18 10:27:56 +0900

    Started on script/prepare.rb (class PrepareUserAuth)
    version=0.0.8.15
    [plugin: not performed]
    README
    - Added an example command to generate model User.
    script/prepare.rb
    - Newly created.

---
+++ script/prepare.rb
@@ -0,0 +1,58 @@
+#! /bin/env ruby
+
+CURRENT_DIRNAME = File.dirname(__FILE__)
+
+require CURRENT_DIRNAME + '/session_store_modifier'
+=begin
+application_controller_modifier.rb
+config_application_modifier.rb
+prepare.rb
+routes_adder.rb
+=end
+
+
+class PrepareUserAuth
+
+  UP = '/..'
+
+  MODIFIERS = [
+    [SessionStoreModifier, :modify],
+  ]
+
+  def initialize
+    rails_root = search_rails_root(CURRENT_DIRNAME)
+    raise RuntimeError, "Cannot find Rails root directory" unless rails_root
+  end
+
+  def prepare
+  end
+
+  private
+
+    def search_rails_root(dirname_start)
+      dirname = dirname_start
+      while File.directory?(dirname)
+        return dirname if rails_root?(dirname)
+        dirname += UP
+      end
+
+      return nil
+    end
+
+    MARKER_FILENAMES_IN_RAILS_ROOT = %w(app config db Gemfile public Rakefile script vendor)
+
+    def rails_root?(dirname)
+      filenames = Dir.entries(dirname)
+      MARKER_FILENAMES_IN_RAILS_ROOT.each do |marker_filename|
+        return false unless filenames.include?(marker_filename)
+      end
+
+      return true
+    end
+end
+
+
+if __FILE__ == $0
+  pua = PrepareUserAuth.new
+end
+
