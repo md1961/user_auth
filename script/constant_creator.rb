db8564b068251f1e8bdff92b232b4e03b87f50af
Naoyuki Kumagai <kumagai@uranus>
2011-02-22 13:29:07 +0900

    Corrected ConstantCreator according to super ModifierOrFileCreator change
    version=0.0.8.44
    script/constant_creator.rb
    - Corrected initializers() to receive argument argv instead of dirname
      and pass it with argument no_modify of true.
      Also modified it to prepare @template_file_contents.
    - Added target_filename() and template_file_contents().
    - Altered make_template_file_contents() to return an Array of lines
      instead of a joined string.

--- script/constant_creator.rb
+++ script/constant_creator.rb
@@ -8,9 +8,10 @@ class ConstantCreator < ModifierOrFileCreator
   TARGET_FILENAME = "config/initializers/00_user_auth_kuma_constant.rb"
   TEMPLATE_FILENAME = File.dirname(__FILE__) + "/templates/00_user_auth_kuma_constant.rb"
 
-  def initialize(dirname)
-    target_filename = dirname + '/' + TARGET_FILENAME
-    super(target_filename, make_template_file_contents, no_modify=true)
+  def initialize(argv)
+    super(argv, no_modify=true)
+
+    @template_file_contents = make_template_file_contents
   end
 
   def modify
@@ -18,6 +19,14 @@ class ConstantCreator < ModifierOrFileCreator
   end
 
   private
+
+    def target_filename
+      return TARGET_FILENAME
+    end
+
+    def template_file_contents
+      return @template_file_contents
+    end
 
     def make_template_file_contents
       lines = Array.new
@@ -27,18 +36,14 @@ class ConstantCreator < ModifierOrFileCreator
         end
       end
 
-      return lines.join
+      return lines
     end
 end
 
 
 if __FILE__ == $0
-  if ARGV.size != 1 || ! File.directory?(ARGV[0])
-    raise ArgumentError, "Specify directory which has '#{ConstantCreator::TARGET_FILENAME}'"
-  end
-
-  um = ConstantCreator.new(ARGV[0])
-  um.modify
-  puts um.message
+  cc = ConstantCreator.new(ARGV)
+  cc.modify
+  puts cc.message
 end
 
