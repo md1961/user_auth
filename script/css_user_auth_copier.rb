bd1e62b3a7a6c382c029a8c390bf7e5557b515f7
Naoyuki Kumagai <kumagai@uranus>
2011-02-22 16:36:38 +0900

    Created CssUserAuthCopier
    version=0.0.8.48
    script/css_user_auth_copier.rb
    - Newly created.
    stylesheets/user_auth.css
    stylesheets/user_auth_kuma.css
    - Renamed to 'user_auth_kuma.css'.

---
+++ script/css_user_auth_copier.rb
@@ -0,0 +1,49 @@
+#! /bin/env ruby
+
+require File.dirname(__FILE__) + '/modifier_or_file_creator'
+
+
+class CssUserAuthCopier < ModifierOrFileCreator
+
+  TARGET_FILENAME = "public/stylesheets/user_auth_kuma.css"
+  TEMPLATE_FILENAME = File.dirname(__FILE__) + "/../stylesheets/user_auth_kuma.css"
+
+  def initialize(argv)
+    super(argv, no_modify=true)
+
+    @template_file_contents = make_template_file_contents
+  end
+
+  def modify
+    return super
+  end
+
+  private
+
+    def target_filename
+      return TARGET_FILENAME
+    end
+
+    def template_file_contents
+      return @template_file_contents
+    end
+
+    def make_template_file_contents
+      lines = Array.new
+      File.open(TEMPLATE_FILENAME, 'r') do |f|
+        f.each do |line|
+          lines << line
+        end
+      end
+
+      return lines
+    end
+end
+
+
+if __FILE__ == $0
+  cuac = CssUserAuthCopier.new(ARGV)
+  cuac.modify
+  puts cuac.message
+end
+
