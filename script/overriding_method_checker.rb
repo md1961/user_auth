f32b5f99158fbb55496e140d4d046a4921acae7e
Naoyuki Kumagai <kumagai@uranus>
2011-03-10 13:56:52 +0900

    Almost completed OverridingMethodChecker
    version=0.7.0.4
    [plugins: not performed]
    script/overriding_method_checker.rb
    - Added H_ROOT_DIRS_AND_NAMESPACES.
    - Implemented check() to display overriding methods.

--- script/overriding_method_checker.rb
+++ script/overriding_method_checker.rb
@@ -1,26 +1,39 @@
 #! /bin/env ruby
 
-PLUGIN_DIR = File.dirname(__FILE__) + '/..'
-
 
 class OverridingMethodChecker
 
-  FILES_WITH_DIR_TO_CHECK = [
+  PLUGIN_LIB_DIR = File.dirname(__FILE__) + '/../lib'
+
+  FILENAMES_WITH_DIR_TO_CHECK = [
     %w(user.rb             app/models     ),
     %w(users_controller.rb app/controllers),
   ]
 
-  ROOT_DIRS_AND_NAMESPACES = [
-    ["."                 , ""              ],
-    [PLUGIN_DIR + "/lib" , "user_auth_kuma"],
-  ]
+  H_ROOT_DIRS_AND_NAMESPACES = {
+    overriding: ["."           , ""              ],
+    overridden: [PLUGIN_LIB_DIR, "user_auth_kuma"],
+  }
+
+  INDENT = "  "
 
   def check
-    FILES_WITH_DIR_TO_CHECK.each do |file, dir|
-      ROOT_DIRS_AND_NAMESPACES.each do |root_dir, namespace|
-        file_full = File.join(root_dir, dir, namespace, file)
-        methods = user_defined_methods(file_full)
-        puts "#{file_full} : #{methods.map { |m| "  " + m.to_s }.join("\n")}"
+    FILENAMES_WITH_DIR_TO_CHECK.each do |filename, dir|
+      h_methods = Hash.new
+      H_ROOT_DIRS_AND_NAMESPACES.each do |name, root_and_ns|
+        root_dir, namespace = root_and_ns
+        full_filename = File.join(root_dir, dir, namespace, filename)
+        h_methods[name] = user_defined_methods(full_filename)
+      end
+
+      overriding_filename = File.join(dir, filename)
+      overriding_methods = Array.new
+      h_methods[:overridden].each do |method|
+        overriding_methods << method if h_methods[:overriding].include?(method)
+      end
+      unless overriding_methods.empty?
+        puts "The following methods in '#{overriding_filename}' are overriding the ones in the plugin"
+        puts overriding_methods.map { |method| INDENT + method.to_s }.join("\n")
       end
     end
   end
@@ -29,9 +42,9 @@ class OverridingMethodChecker
 
     RE_METHOD = /\A\s*def\s+((?:self\.)?\w+\??)/
 
-    def user_defined_methods(file)
+    def user_defined_methods(filename)
       methods = Array.new
-      File.open(file, 'r') do |fd|
+      File.open(filename, 'r') do |fd|
         fd.each do |line|
           if line =~ RE_METHOD
             methods << $1.intern
