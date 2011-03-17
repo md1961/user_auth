19f1a896aa9b18436c6f26e48ff1b1912bc1d737
Naoyuki Kumagai <kumagai@uranus>
2011-03-17 14:36:15 +0900

    Add functionality to disable login
    version=0.7.4.0
    README
    - Add explanation how to disable login.
    - Update the files to be installed.
    app/views/sessions/login_disabled.html.erb
    - Newly created.
    lib/app/controllers/sessions_controller.rb
    - Add new().
    locales/ja.yml
    - Add "login_disabled" under "helpers.page_title.sessions".

--- lib/app/controllers/sessions_controller.rb
+++ lib/app/controllers/sessions_controller.rb
@@ -1,6 +1,24 @@
 # モデル Session の処理を行うコントローラ
 class SessionsController < ApplicationController
 
+  DEFAULT_LOGIN_ALLOWANCE = {
+    :login_enabled  => true ,
+    :login_disabled => false,
+  }
+
+  # ログイン画面を表示する
+  # Constant.get(:login_enabled) が false を返すか、Constant.get(:login_disabled) が
+  # true を返す場合は、ログインができない旨を表示する。
+  def new
+    login_allowance = Hash.new { |hash, key| DEFAULT_LOGIN_ALLOWANCE[key] }
+    [:login_enabled, :login_disabled].each do |key|
+      login_allowance[key] = Constant.get(key) if Constant.has_key?(key)
+    end
+    is_login_disallowed = ! login_allowance[:login_enabled] || login_allowance[:login_disabled]
+
+    render is_login_disallowed ? :login_disabled : :new
+  end
+
   # ログイン認証を行い、成功すれば Session にユーザIDを設定する。失敗した場合はログイン画面に戻る<br />
   # <em>params[:name]</em> : ユーザ名<br />
   # <em>params[:password]</em> : パスワード
