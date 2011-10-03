5ed9751942991009e17c3547d12a53b375d2388b
Naoyuki Kumagai <kumagai@mimas>
2011-10-03 14:52:04 +0900

    Fix UserAuthTest for the constant name
    test/user_auth_test.rb
    - Fix to use USER_AUTH_DIRECTORIES_TO_LOAD_FROM instead of
      DIRECTORIES_TO_LOAD_FROM.

--- test/user_auth_test.rb
+++ test/user_auth_test.rb
@@ -5,7 +5,7 @@ class UserAuthTest < ActiveSupport::TestCase
   def test_autoload_paths
     autoload_paths = ActiveSupport::Dependencies.autoload_paths
 
-    DIRECTORIES_TO_LOAD_FROM.each do |dir|
+    USER_AUTH_DIRECTORIES_TO_LOAD_FROM.each do |dir|
       dir_fullpath = File.expand_path("#{File.dirname(__FILE__)}/../lib/app/#{dir}")
       assert(autoload_paths.include?(dir_fullpath), "'{dir_fullpath}' should be in autoload_paths")
     end
