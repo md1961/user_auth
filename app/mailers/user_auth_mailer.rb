4263cc76ffc3bb83ba96efeb0ef029cdab03b725
Naoyuki Kumagai <kumagai@namaka>
2013-05-30 11:07:19 +0900

    Fix user_auth_mailer.rb to use correct system name for mail subjects
    app/mailers/user_auth_mailer.rb
    - Fix all the methods to embed SYSTEM_NAME to the subjects.
    locales/ja.yml
    - Alter "user_auth_mailer.*.subject" to have embedding system name.

--- app/mailers/user_auth_mailer.rb
+++ app/mailers/user_auth_mailer.rb
@@ -7,7 +7,8 @@ class UserAuthMailer < ActionMailer::Base
     @user = user
     @temporary_password = temporary_password
 
-    mail :to => user.email
+    mail :to => user.email, \
+         :subject => t("user_auth_mailer.notify_password_reset.subject") % {system_name: SYSTEM_NAME}
   end
 
   def notify_user_creation(user, temporary_password)
@@ -15,13 +16,15 @@ class UserAuthMailer < ActionMailer::Base
     @temporary_password = temporary_password
     @url_for_system = UserAuthKuma::Constant::Mailer::URL_FOR_SYSTEM
 
-    mail :to => user.email
+    mail :to => user.email, \
+         :subject => t("user_auth_mailer.notify_user_creation.subject") % {system_name: SYSTEM_NAME}
   end
 
   def notify_email_change(user)
     @user = user
 
-    mail :to => user.email
+    mail :to => user.email, \
+         :subject => t("user_auth_mailer.notify_email_change.subject") % {system_name: SYSTEM_NAME}
   end
 end
 
